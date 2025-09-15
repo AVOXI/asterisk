f8a8a044999950beacf16d6900ca09429bfa65ea
charles.barry <charles.barry@avoxi.com>
2025-09-15 16:08:21 -0400

    fix

--- main/bridge_channel.c
+++ main/bridge_channel.c
@@ -1199,14 +1199,14 @@ int ast_bridge_channel_avoxi_purge_all_queues(struct ast_bridge *bridge, enum as
 	}
 
 	AST_LIST_TRAVERSE(&bridge->channels, bridge_channel, entry) {
-		ast_log(LOG_DEBUG, "AVOXI: Purging bridge channel %s\n", ast_channel_name(bridge_channel->chan));
+		ast_log(LOG_DEBUG, "AVOXI: Purging bridge channel %s\n", bridge->uniqueid);
 		/* Purge bridge channel write queue */
 		total_purged += ast_bridge_channel_avoxi_purge_queue(bridge_channel, frame_type_filter);
 		
 		/* Purge actual channel read queue */
 		chan = ast_bridge_channel_get_chan(bridge_channel);
 		if (chan) {
-			ast_log(LOG_DEBUG, "AVOXI: Purging channel %s\n", ast_channel_name(chan));
+			ast_log(LOG_DEBUG, "AVOXI: Purging channel %s on bridge %s\n", ast_channel_name(chan), bridge->uniqueid);
 			total_purged += ast_channel_avoxi_purge_read_queue(chan, frame_type_filter);
 			ao2_ref(chan, -1);
 		}
